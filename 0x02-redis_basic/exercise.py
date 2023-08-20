ec6ed9372a06f53389dbaf68a49b6e3a31ab0559
Martins100-Tmd <Olaitanfabayo@gmail.com>
2023-08-20 14:52:24 +0000

    Redis basics

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,13 +4,12 @@ Task 0's module
 '''
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
     '''redis cache class
     '''
-
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb(True)
@@ -21,3 +20,21 @@ class Cache:
         uniqueId =str(uuid.uuid4())
         self._redis.set(uniqueId, data)
         return uniqueId
+
+    def get(self, key: str, fn: Callable) -> Union[str, float, int, bytes]:
+        '''get data from redis
+        '''
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str):
+        '''
+        string return method
+        '''
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        '''
+        int return method
+        '''
+        return self.get(key, lambda x: int(x))
